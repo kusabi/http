kusabi/http

commit fd37fc80fcf8c6035e7d13ec3d43ab6b6ac79787

Removed the use of a else statement as per adivements of codacy

src/AbstractMessage.php
@@ -243,13 +243,12 @@ abstract class AbstractMessage implements MessageInterface
         if (!$this->isValidHeaderKey($name)) {
             throw new InvalidHeaderKeyException($name);
         }
+        $this->setHeader($name, $value);
         if ($this->hasHeader($name)) {
             $this->setHeader($this->headerKeys[$this->normaliseHeaderKey($name)], array_merge(
                 (array) $this->getHeader($name),
                 (array) $value
             ));
-        } else {
-            $this->setHeader($name, $value);
         }
     }
 

commit a542922f2e33e29983fe54bef73cb4f13357e93d

Fixed a bug introduced in commit fd37fc80fcf8c6035e7d13ec3d43ab6b6ac79787

src/AbstractMessage.php
@@ -243,13 +243,14 @@ abstract class AbstractMessage implements MessageInterface
         if (!$this->isValidHeaderKey($name)) {
             throw new InvalidHeaderKeyException($name);
         }
-        $this->setHeader($name, $value);
         if ($this->hasHeader($name)) {
             $this->setHeader($this->headerKeys[$this->normaliseHeaderKey($name)], array_merge(
                 (array) $this->getHeader($name),
                 (array) $value
             ));
+            return;
         }
+        $this->setHeader($name, $value);
     }
 
     /**